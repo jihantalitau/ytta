36b77686b68c732da4ca4ca894009068a482df01
jihantalitau <nokos8034@gmail.com>
2025-12-25 01:17:06 +0700

    Delete client directory

--- client/RespawnButton.client.lua
+++
@@ -1,8 +0,0 @@
--- StarterGui/RespawnGui/RespawnButton/LocalScript
-
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local event = ReplicatedStorage:WaitForChild("RespawnAllEvent")
-
-script.Parent.MouseButton1Click:Connect(function()
-	event:FireServer()
-end)
