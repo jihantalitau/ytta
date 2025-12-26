3dccda8c286943e3154ee2df46bcc8d18aa0b96b
jihantalitau <nokos8034@gmail.com>
2025-12-26 12:49:53 +0700

    Update and rename eaa.lua to eaaa.lua

--- eaaa.lua
+++ eaaa.lua
@@ -11,11 +11,11 @@ local function addCoins(amount)
     local leaderstats = localPlayer:FindFirstChild("leaderstats")
     if leaderstats then
         local coins = leaderstats:FindFirstChild(coinName)
-        if coins then
+        if coins and coins:IsA("IntValue") then
             coins.Value = coins.Value + amount
             print("Menambah " .. amount .. " " .. coinName .. " ke " .. localPlayer.Name)
         else
-            print("Tidak dapat menemukan " .. coinName .. " dalam Leaderstats.")
+            print("Tidak dapat menemukan " .. coinName .. " dalam Leaderstats atau bukan IntValue.")
         end
     else
         print("Tidak dapat menemukan Leaderstats.")
@@ -27,11 +27,11 @@ local function addRP(amount)
     local leaderstats = localPlayer:FindFirstChild("leaderstats")
     if leaderstats then
         local rp = leaderstats:FindFirstChild(rpName)
-        if rp then
+        if rp and rp:IsA("IntValue") then
             rp.Value = rp.Value + amount
             print("Menambah " .. amount .. " " .. rpName .. " ke " .. localPlayer.Name)
         else
-            print("Tidak dapat menemukan " .. rpName .. " dalam Leaderstats.")
+            print("Tidak dapat menemukan " .. rpName .. " dalam Leaderstats atau bukan IntValue.")
         end
     else
         print("Tidak dapat menemukan Leaderstats.")
@@ -39,5 +39,6 @@ local function addRP(amount)
 end
 
 -- Jalankan fungsi ketika script diexecute
-addCoins(1000000000) -- Menambah 100 coin
-addRP(500000000)      -- Menambah 50 RP
+addCoins(1000000)  -- Menambah 100 coin
+addRP(5000000)      -- Menambah 50 RP
+
